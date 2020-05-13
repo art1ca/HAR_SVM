aac8e0f9e77f14e83af9a690cd62ba0e16f0d24f
art1ca <fabrizio-098@hotmail.com>
2020-05-13 16:37:30 -0500

    pandasTest

---
+++ pandas.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed May 13 16:30:24 2020
+
+@author: WIN81
+"""
+
+import pandas as pd
+
+df= pd.DataFrame({'animal': ['alligator', 'bee', 'falcon', 'lion',
+                   'monkey', 'parrot', 'shark', 'whale', 'zebra']})
+df
